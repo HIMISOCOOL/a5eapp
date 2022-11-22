6b1447a7d97d361c394acdf9026361625a452bf3
Matthew Meehan <lololirpro@gmail.com>
2022-11-22 22:35:30 +1300

    added ability score to character page

--- src/views/Character.vue
+++ src/views/Character.vue
@@ -1,11 +1,98 @@
 <template>
     <main class="grid grid-cols-5">
-        <section class="ability-scores col-span-1">Ability Scores</section>
         <section class="statistics col-span-1">Statistics</section>
-        <section class="destiny col-span-3">Destiny</section>
+        <section>
+            <h3>Ability Scores</h3>
+            <l-grid class="ability-scores">
+                <AbilityScore
+                    v-for="(value, key) in abilityScore"
+                    :key="key"
+                    v-bind="value"
+                />
+            </l-grid>
+        </section>
+        <section class="skills col-span-3">Passives</section>
         <section class="skills col-span-3">Skills</section>
     </main>
 </template>
+<script lang="ts">
+import { reactive, toRefs, defineComponent, ref } from 'vue';
+
+import AbilityScore from '@/components/abilityScores/abilityscore.vue';
+import LGrid from '@/components/layout/grid.vue';
+
+const getAbilityScore = (name: string, score: any) => {
+    const abbreviation = name.substring(0, 3).toUpperCase();
+    return {
+        name,
+        abbreviation,
+        ...score,
+    };
+};
+const getAbilityScores = (abilityScores: any) => {
+    const completeScores = Object.keys(abilityScores).map(key =>
+        getAbilityScore(key, abilityScores[key])
+    );
+    return completeScores.reduce(
+        (prev, curr) => ({ ...prev, [curr.name]: curr }),
+        {}
+    );
+};
+
+export default defineComponent({
+    name: 'Character',
+    components: {
+        AbilityScore,
+        LGrid,
+    },
+    setup(props) {
+        const proficiencyBonus = ref(0);
+        const character = reactive({
+            abilityScore: getAbilityScores({
+                Strength: {
+                    score: 0,
+                    saveBonus: 0,
+                    proficient: false,
+                    proficiencyBonus,
+                },
+                Dexterity: {
+                    score: 0,
+                    saveBonus: 0,
+                    proficient: false,
+                    proficiencyBonus,
+                },
+                Constitution: {
+                    score: 0,
+                    saveBonus: 0,
+                    proficient: false,
+                    proficiencyBonus,
+                },
+                Wisdom: {
+                    score: 0,
+                    saveBonus: 0,
+                    proficient: false,
+                    proficiencyBonus,
+                },
+                Inteligence: {
+                    score: 0,
+                    saveBonus: 0,
+                    proficient: false,
+                    proficiencyBonus,
+                },
+                Charisma: {
+                    score: 0,
+                    saveBonus: 0,
+                    proficient: false,
+                    proficiencyBonus,
+                },
+            }),
+        });
+        return {
+            ...toRefs(character),
+        };
+    },
+});
+</script>
 <style scoped>
 main {
     grid-template-areas:
